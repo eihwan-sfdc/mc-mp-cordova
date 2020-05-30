f194e90489b66746afbd99051daa830c48c26718
EihwanKim <cloz2me@gmail.com>
2020-05-30 20:55:48 +0900

    .

---
+++ platforms/ios/NotificationService/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by 金義桓 on 2020/05/30.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
